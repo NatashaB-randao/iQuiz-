e336bbcc116ef423d625f8124b89b8aaa4ba8578
NatashaB-randao <natashabrandao57@hotmail.com>
2023-05-12 13:43:26 -0300

    Conectando os botões e ajustando cantos arredondados

--- iQUiz/iQUiz/QuestaoViewController.swift
+++ iQUiz/iQUiz/QuestaoViewController.swift
@@ -11,12 +11,22 @@ class QuestaoViewController: UIViewController {
 
     @IBOutlet weak var tituloQuestaoLabel: UILabel!
     
-    @IBAction func respostaBotaoPressionado(_ sender: Any) {
+    @IBOutlet var botoesRespostas: [UIButton]!
+    @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+        print(sender.tag)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configurarLayout()
+    }
+    
+    func configurarLayout() {
         navigationItem.hidesBackButton = true
+        for botao in botoesRespostas {
+            botao.layer.cornerRadius = 12.0
+        }
+        
     }
     
 
