e3dfcdb8bbc59a3e0a8f70b15a1244ab9695ade4
NatashaB-randao <natashabrandao57@hotmail.com>
2023-05-13 12:46:40 -0300

    Configurando a resposta da questão

--- iQUiz/iQUiz/QuestaoViewController.swift
+++ iQUiz/iQUiz/QuestaoViewController.swift
@@ -16,7 +16,13 @@ class QuestaoViewController: UIViewController {
     
     @IBOutlet var botoesRespostas: [UIButton]!
     @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
-        print(sender.tag)
+        let usuarioAcertouResposta = questoes[numeroQuestao].respostaCorreta == sender.tag
+        
+        if usuarioAcertouResposta {
+            pontuacao += 1
+            print("O usuário acertou!")
+        }
+
     }
     
     
@@ -40,6 +46,10 @@ class QuestaoViewController: UIViewController {
     
     func configurarQuestao() {
         tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
+        for botao in botoesRespostas {
+            let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
+            botao.setTitle(tituloBotao, for: .normal)
+        }
     }
 
    
