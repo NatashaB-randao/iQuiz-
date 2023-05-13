5cb6a0c655dabc730cb4cb69b20b5dfbbbf38afb
NatashaB-randao <natashabrandao57@hotmail.com>
2023-05-13 13:59:34 -0300

    Construindo a tela de desempenho.

---
+++ iQUiz/iQUiz/DesempenhoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DesempenhoViewController.swift
+//  iQUiz
+//
+//  Created by Natasha Brandão on 13/05/23.
+//
+
+import UIKit
+
+class DesempenhoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
