9a90e9ae05e21d2b5a7bcfc78527be898d0919af
NatashaB-randao <natashabrandao57@hotmail.com>
2023-05-13 15:19:10 -0300

    Mostrando o desempenho e organizando o código

--- iQUiz/iQUiz/ViewController/DesempenhoViewController.swift
+++ iQUiz/iQUiz/ViewController/DesempenhoViewController.swift
@@ -24,12 +24,20 @@ class DesempenhoViewController: UIViewController {
         super.viewDidLoad()
 
         configurarLayout()
+        configurarDesempenho()
     }
     
     func configurarLayout() {
+        navigationItem.hidesBackButton = true
         botaoReiniciarQuiz.layer.cornerRadius = 12.0
     }
     
+    func configurarDesempenho() {
+        guard let pontuacao = pontuacao else { return }
+        resultadoLabel.text = "Você acertou \(pontuacao) de \(questoes.count) questões."
+        let percentual = (pontuacao * 100) / questoes.count
+        percentualLabel.text = "Percentual final: \(percentual)%"
+    }
 
     /*
     // MARK: - Navigation
